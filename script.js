3bb0c9ea0ec73b895887bb3dfac7b0d5550977bd
Taha S. ASAN <tahaasann@gmail.com>
2024-02-10 16:41:29 +0300

    v0.0.3

--- script.js
+++ script.js
@@ -16,6 +16,24 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [
+    {
+        name: "stick",
+        "power": 5
+    },
+    {
+        name: "dagger",
+        "power": 30
+    },
+    {
+        name: "claw hammer",
+        "power": 50
+    },
+    {
+        name: "sword",
+        "power": 100
+    }
+];
 const locations = [
     {
         name: "town square",
@@ -75,11 +93,18 @@ function buyHealth() {
         healthText.innerText = health;
     }
     else{
-        
+        text.innerText = "You do not have enough gold to buy health.";
     }
 }
 function buyWeapon() {
+    if(gold >= 30){
+        gold-=30;
+        currentWeapon++;
+        goldText.innerText = gold;
+        let newWeapon = weapons[currentWeapon].name;
+        text.innerText = "You now have a " + newWeapon + ".";
 
+    }
 }
 
 function fightSlime() {
